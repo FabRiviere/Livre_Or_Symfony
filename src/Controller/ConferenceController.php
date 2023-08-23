d56649f7a75b35ea5686a477fd0bd1812f6c9744
Fab Riviere <fabien@activateurweb.com>
2023-08-23 19:12:25 +0200

    Ajout du template Twig index.html.twig, pour affichage de la page d'accueil, et modification de la méthode de contrôleur index(). #11

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,25 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
         // return $this->render('conference/index.html.twig', [
         //     'controller_name' => 'ConferenceController',
         // ]);
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    <img src="/images/under-construction.gif">
-                </body>
-            </html>
-            EOF
-        );
+        // return new Response(<<<EOF
+        //     <html>
+        //         <body>
+        //             <img src="/images/under-construction.gif">
+        //         </body>
+        //     </html>
+        //     EOF
+        // );
+        return new Response ($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 }
