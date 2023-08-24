632336ae2c5f17af3418a96b256063d5c8e1f55a
Fab Riviere <fabien@activateurweb.com>
2023-08-24 17:50:19 +0200

    #24 Création d'un formulaire pour les commentaires utilisateurs. Personnalisation du formulaire, contraintes sur les champs de l'entité Comment, Soumission et traitement du formulaire dans le contrôleur ConferenceController.php, et affichage du formulaire brut (pour l'instant) dans le template show.html.twig (page d'une conference). #25 Gestion de l'upload des photos/images sur les commentaires. Paramètre de configuration du dossier de stockage des fichiers uploadés. Modification du contrôleur pour le traitement des fichiers reçus dans le formulaire. Modification du contrôleur CommentCrudController.php pour affichage des photos sur la liste des commentaires. Voir issues traitées.

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
@@ -45,7 +46,11 @@ class CommentCrudController extends AbstractCrudController
         yield TextField::new('author');
         yield EmailField::new('email');
         yield TextareaField::new('text')->hideOnIndex();
-        yield TextField::new('photoFilename')->onlyOnIndex();
+        // yield TextField::new('photoFilename')->onlyOnIndex();
+        yield ImageField::new('photoFilename')
+                    ->setBasePath('/uploads/photos')
+                    ->setLabel('Photo')
+                    ->onlyOnIndex();
 
         $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions([
             'years' => range(date('Y'), date('Y') + 5),
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $entityManager)
+    {        
+    }
+    
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
@@ -32,8 +40,29 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository,
+                            #[Autowire('%photo_dir%')] string $photoDir): Response
     {
+        $comment = new Comment();
+        $formComment = $this->createForm(CommentType::class, $comment);
+        $formComment->handleRequest($request);
+
+        if ($formComment->isSubmitted() && $formComment->isValid()) {
+            $comment->setConference($conference);
+
+            //! Traitement des photos
+            if ($photo = $formComment['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                $photo->move($photoDir, $filename);
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+        
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         
@@ -42,6 +71,7 @@ class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $formComment,
         ]);
     }
 }
