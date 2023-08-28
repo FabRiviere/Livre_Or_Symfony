3650ce2566026e72b8c7e789b47651b9ee965bf9
Fab Riviere <fabien@activateurweb.com>
2023-08-28 20:40:47 +0200

    #36, #37, #38, Ajout d'une propriété state à l'entité Comment. Gestion de l'asynchrone avec le composant Messenger et mise en place de travailleurs pour surveiller les nouveaux commentaires avant publication

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,38 +3,29 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
-use App\Entity\Conference;
 use App\Form\CommentType;
+use App\Entity\Conference;
+use App\Service\SpamChecker;
+use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
-use App\Service\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $entityManager)
+    public function __construct(private EntityManagerInterface $entityManager, private MessageBusInterface $bus)
     {        
     }
     
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
-        // return $this->render('conference/index.html.twig', [
-        //     'controller_name' => 'ConferenceController',
-        // ]);
-        // return new Response(<<<EOF
-        //     <html>
-        //         <body>
-        //             <img src="/images/under-construction.gif">
-        //         </body>
-        //     </html>
-        //     EOF
-        // );
         return $this->render('conference/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
         ]);
@@ -42,7 +33,7 @@ class ConferenceController extends AbstractController
 
     #[Route('/conference/{slug}', name: 'conference')]
     public function show(Request $request, Conference $conference, CommentRepository $commentRepository,
-                            #[Autowire('%photo_dir%')] string $photoDir, SpamChecker $spamChecker): Response
+                            #[Autowire('%photo_dir%')] string $photoDir): Response
     {
         $comment = new Comment();
         $formComment = $this->createForm(CommentType::class, $comment);
@@ -59,6 +50,7 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+            $this->entityManager->flush();
 
             //! vérification si présence de spam avant de stocker les commentaires en DB
             $context = [
@@ -67,11 +59,14 @@ class ConferenceController extends AbstractController
                 'referrer' => $request->headers->get('referer'),
                 'permalink' => $request->getUri(),
             ];
-            if (2 === $spamChecker->getSpamScore($comment, $context)) {
-                throw new \RuntimeException('Blatant spam, go away !! 😵‍💫😟');
-            }
+            // ! mis en commentaire pour utilisation du MessageBusInterface à la place de SpamChecker
+            // if (2 === $spamChecker->getSpamScore($comment, $context)) {
+            //     throw new \RuntimeException('Blatant spam, go away !! 😵‍💫😟');
+            // }
 
-            $this->entityManager->flush();
+            // $this->entityManager->flush();
+            // ! Utilisation du MessageBusInterface
+            $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
